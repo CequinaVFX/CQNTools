a4aa5a5161bd7afe0be6df8ac57a9618925595b9
Luciano Cequinel <lucianocequinel@gmail.com>
2021-01-09 12:33:30 -0300

    FrameBreakdown

--- FrameBreakdown/FrameBreakdown.py
+++ FrameBreakdown/FrameBreakdown.py
@@ -1,23 +1,13 @@
 ##############################################################################
 #                                                                            #
 # Breakdown Frame Exporter                                                   #
-# V 1.0.0                                                                    #
-# Release August 01 2020                                                     #
+# V 1.1.0                                                                    #
+# Release January 10 2021                                                    #
 #                                                                            #
 # Created by Luciano Cequinel (vimeo.com/cequinavfx)                         #
 # to report bugs or suggestions lucianocequinel@gmail.com                    #
 #                                                                            #
 ##############################################################################
-
-
-
-'''
-To improve
-- position of dot
-- name of write node, to prevent error message
-- to create a section to config some stuffs, like file extension, channels, premult
-
-'''
 
 
 '''
@@ -45,7 +35,7 @@ def FrameBreakdown():
         #Get current frame
         curFrame = (nuke.frame())
 
-        folderName = 'bkdown_' + str(curFrame)# + '/'
+        folderName = 'breakdown_frame_' + str(curFrame)# + '/'
 
         #Get current script folder and create a folder called 'frames'
         saveDir = ('/'.join(nuke.root().name().split("/")[0:-1])+'/')
@@ -63,8 +53,10 @@ def FrameBreakdown():
             file_count = len(files) + 1
 
         #Create a filename to save
-        saveDir = ('%s/%03d_frame_%s_%s.exr' %(saveDir, file_count, curFrame, selNode.name()))
- 
+        #saveDir = ('%s/%03d_frame_%s_%s.exr' %(saveDir, file_count, curFrame, selNode.name()))
+
+        saveDir = ('%s/%03d_%s_frame_%s.exr' %(saveDir, file_count, selNode.name(), curFrame))
+
         #Get original state of Proxy Mode in Project Settings
         origProxyMode = nuke.root().proxy()
         #Set Proxy Mode to Off for render purpose
@@ -73,17 +65,30 @@ def FrameBreakdown():
         #Create a Write node
         nWrite = nuke.createNode('Write')
         nWrite['name'].setValue('WriteFrame')
+        nWrite.setName('WriteFrame_' + selNode.name() , uncollide=True)
+
         nWrite['channels'].setValue('rgba')
+
         nWrite['file'].setValue (saveDir)
         nWrite['file_type'].setValue ('exr')
         nWrite['create_directories'].setValue(True)
 
-        #Create Dot do mark were you render a frame
+        #Create Dot do mark where you render a frame
         nDot = nuke.createNode('Dot')
         nDot['label'].setValue(folderName)
+
         nDot['tile_color'].setValue(35)
         nDot['note_font_size'].setValue(10)
         nDot['note_font_color'].setValue(15)
+
+        if selNode.Class() == 'Dot':
+            nDot.setXpos( int (selNode['xpos'].getValue() ) )
+            nDot.setYpos( int (selNode['ypos'].getValue() + 40 ) )
+        else:
+            nDot.setXpos( int (selNode['xpos'].getValue() + 34 ) )
+            nDot.setYpos( int (selNode['ypos'].getValue() + 40 ) )
+
+
         nDot.setInput(0, selNode)
         nDot.hideControlPanel()
 
@@ -96,11 +101,14 @@ def FrameBreakdown():
 
         #Set Proxy Mode to Original state
         nuke.root()['proxy'].setValue(origProxyMode)
+
     else:
         nuke.message('Select just one node, dude')
 
 
+
+
 #Add a menu and assign a shortcut
 toolbar = nuke.menu('Nodes')
-cqnTools = toolbar.addMenu('CequinaTools', 'cqnTools.png')
+cqnTools = toolbar.addMenu('CQNTools', 'Write.png')
 cqnTools.addCommand('Frame Breakdown', 'FrameBreakdown.FrameBreakdown()', 'F12', icon='Write.png')
